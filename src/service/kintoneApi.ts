f7fd9f9695b619c87b9162dd2ba54df1e01dbb7c
slgu <slgu@cybozu.cn>
2023-09-23 15:58:13 +0800

    筛选的ts sample

--- src/service/kintoneApi.ts
+++ src/service/kintoneApi.ts
@@ -10,4 +10,14 @@ export class KintoneApi {
   public async getAllRecords(app: string) {
     return await this.client.record.getAllRecords<GoodListAppRecord>({ app })
   }
+
+  public async getRecords(
+    app: string,
+    fields?: Array<keyof kintone.types.Fields>,
+    query?: string,
+    totalCount?: boolean,
+  ) {
+    const result = await this.client.record.getRecords<Partial<GoodListAppRecord>>({ app, fields, query, totalCount })
+    return result.records
+  }
 }
--- src/views/Space.vue
+++ src/views/Space.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>
-    <h1>Space</h1>
-  </div>
+  <List v-model:loading="loading" :finished="finished" @load="onLoad">
+    <Card v-for="(item, index) in list" :key="index" :num="item.num?.value" :price="item.price?.value"
+      :title="item.title?.value" :thumb="item.thumb?.value" />
+  </List>
 </template>
 
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import { Card, List } from 'vant'
+import { KintoneApi } from '@/service/kintoneApi'
+import type { GoodListAppRecord } from '@/types/restApiRecords'
+
+const list: Ref<Partial<GoodListAppRecord>[]> = ref([])
+
+const loading = ref(false);
+const finished = ref(false);
+
+const onLoad = () => {
+  const kintoneClient = new KintoneApi()
+  const appId = import.meta.env.VITE_APP_ID
+  const fields: (keyof kintone.types.Fields)[] = ['num', 'price', 'title', 'thumb']
+  const query = 'order by $id desc'
+  kintoneClient.getRecords(appId, fields, query).then((res) => {
+    list.value = res
+    loading.value = false;
+    finished.value = true;
+  })
+};
+</script>
 
